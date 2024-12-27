b2ee5aa4c0c90b1ec08b3e06c033ca14d098920b
acosta <acosta@digiboard.com>
2024-12-27 14:00:04 -0400

    corrigindo erro ao dar GET

--- models/employee_model.js
+++ models/employee_model.js
@@ -4,10 +4,21 @@
 module.exports = (sequelize, DataTypes) => {
     const Employee = sequelize.define("Employee", {
         name: DataTypes.STRING,
-        hire_date: DataTypes.DATE,
+        hire_date: {
+            type:DataTypes.DATE, 
+            field: "hire_date"
+        },
         position_id: {
             type: DataTypes.INTEGER,
             field: "position_id"
+        },
+        createdAt:{
+            type:DataTypes.DATE,
+            field:"created_at"
+        },
+        updatedAt:{
+            type:DataTypes.DATE,
+            field:"updated_at"
         }
     });
     Employee.associate = function(models){
