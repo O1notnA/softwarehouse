55fd55556be3efc83997c32689e67153313a3b0b
acosta <acosta@digiboard.com>
2024-12-27 13:49:52 -0400

    corrigindo createdAt e updatedAt

--- models/position_model.js
+++ models/position_model.js
@@ -5,6 +5,14 @@ module.exports = (sequelize, DataTypes) => {
     const Positions = sequelize.define("Position", {
         title: DataTypes.STRING,
         salary: DataTypes.FLOAT,
+        createdAt:{
+            type:DataTypes.DATE,
+            field:"created_at"
+        },
+        updatedAt:{
+            type:DataTypes.DATE,
+            field:"updated_at"
+        }
     });
     Positions.associate = function(models){
         Positions.hasMany(models.Employee, {foreignKey:'position_id'})
