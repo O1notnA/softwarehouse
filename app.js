8f4a3280ce8579a9d6c13b85cbbf535631110b4b
O1notnA <antonioeduardo789@gmail.com>
2024-12-27 21:55:50 -0400

    adicionando a rota user

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const positionRouter = require("./routes/position_route")
 const employeeRouter = require("./routes/employee_route")
+const userRouter = require("./routes/user_router")
+
+
 const app = express();
 
+app.use(express.json());
 
-app.use("/positions", positionRouter)
-app.use("/employees", employeeRouter)
+app.use("/positions", positionRouter);
+app.use("/employees", employeeRouter);
+app.use("/user", userRouter);
+
+
 app.listen(8000, ()=>{
     console.log("aplicação iniciada");
 });
