f67007f537df27c73af6ad55bd8296afd2f42b1c
O1notnA <antonioeduardo789@gmail.com>
2024-12-27 22:14:32 -0400

    adicionando middleware

--- routes/employee_route.js
+++ routes/employee_route.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const employeeController = require("../controllers/employee_controller")
+const authMiddleware = require("../middleware/middleware")
 
-router.get("/", employeeController.findAllEmployees)
-
-router.post("/", employeeController.createEmployee)
-
-router.put("/:id", employeeController.updateEmployee)
-
-router.delete("/:id", employeeController.deleteEmployee)
+router.get("/",authMiddleware, employeeController.findAllEmployees);
+router.post("/",authMiddleware, employeeController.createEmployee);
+router.put("/:id",authMiddleware, employeeController.updateEmployee);
+router.delete("/:id",authMiddleware, employeeController.deleteEmployee);
 
 module.exports = router;
