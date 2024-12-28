1650c017d1457a4069f5b8b1cd324cbff81b15bb
O1notnA <antonioeduardo789@gmail.com>
2024-12-27 20:09:46 -0400

    removendo lógica das rotas

--- routes/employee_route.js
+++ routes/employee_route.js
@@ -1,65 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {Employee, Position} = require("../models")
+const employeeController = require("../controllers/employee_controller")
 
-router.get("/", async(req, res)=>{
-    try{
-        const employees = await Employee.findAll({
-            include: [Position],
-        });
-        res.json(employees);
-    }
-    catch(err){
-        res.status(500).json({message:"Erro de banco de dados", error: err});
-    }
-})
+router.get("/", employeeController.findAllEmployees)
 
-router.post("/", async(req, res)=>{
-    try{
-        await Employee.create(req.body);
-        res.status(201);
-    }
-    catch(err){
-        res.status(500).json({message:"Erro de banco de dados", error: err});
-    }
-})
+router.post("/", employeeController.createEmployee)
 
-router.put("/:id", async(req, res)=>{
-    try{
-        const [employee] = await Employee.update(req.body, {
-            where:{id: req.params.id}
-        });
-        if (employee){
-            const newEmployee = await Employee.findOne({
-                where:{id:req.params.id}
-            });
-            res.json(newEmployee);
-        }
-        else{
-            throw new Error("Employee não identificada");
-        }
-    }
-    catch(err){
-        res.status(500).json({message:"Erro de banco de dados", error: err});
-    }
-})
+router.put("/:id", employeeController.updateEmployee)
 
-router.delete("/:id", async(req, res)=>{
-    try{
-        const employeeDeleted = await Employee.destroy({
-            where: {id: req.params.id}});
-
-        if (employeeDeleted){
-            res.status(204).send;
-        }
-        else{
-            throw new Error("Employee não identificada");
-        }
-        res.json(employees);
-    }
-    catch(err){
-        res.status(500).json({message:"Erro de banco de dados", error: err});
-    }
-})
+router.delete("/:id", employeeController.deleteEmployee)
 
 module.exports = router;
--- routes/position_route.js
+++ routes/position_route.js
@@ -2,63 +2,14 @@ const express = require('express');
 const router = express.Router();
 const {Position} = require("../models");
 const { where } = require('sequelize');
+const positionController = require("../controllers/position_controller");
 
-router.get("/", async(req, res)=>{
-    try{
-        const positions = await Position.findAll();
-        res.json(positions);
-    }
-    catch(err){
-        res.status(500).json({message:"Erro de banco de dados", error: err});
-    }
-})
+router.get("/", positionController.findAllPositions);
 
-router.post("/", async(req, res)=>{
-    try{
-        await Position.create(req.body);
-        res.status(201);
-    }
-    catch(err){
-        res.status(500).json({message:"Erro de banco de dados", error: err});
-    }
-})
+router.post("/", positionController.createPosition);
 
-router.put("/:id", async(req, res)=>{
-    try{
-        const [position] = await Position.update(req.body, {
-            where:{id: req.params.id}
-        });
-        if (position){
-            const newPosition = await Position.findOne({
-                where:{id:req.params.id}
-            });
-            res.json(newPosition);
-        }
-        else{
-            throw new Error("Position não identificada");
-        }
-    }
-    catch(err){
-        res.status(500).json({message:"Erro de banco de dados", error: err});
-    }
-})
+router.put("/:id", positionController.updatePosition)
 
-router.delete("/:id", async(req, res)=>{
-    try{
-        const positionDeleted = await Position.destroy({
-            where: {id: req.params.id}});
-
-        if (positionDeleted){
-            res.status(204).send;
-        }
-        else{
-            throw new Error("Position não identificada");
-        }
-        res.json(positions);
-    }
-    catch(err){
-        res.status(500).json({message:"Erro de banco de dados", error: err});
-    }
-})
+router.delete("/:id", positionController.deletePosition)
 
 module.exports = router;
